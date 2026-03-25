c160d1447017d9be4726d743d42dd042055a77bd
Junsu0620 <150302819+Junsu0620@users.noreply.github.com>
2026-03-25 22:23:20 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+void int add(int a , int b) {
+    printf("%d + %d = %d\n", a, b, (a + b));
+} 
+void int sub(int a , int b) {
+    printf("%d - %d = %d\n", a, b, (a - b));
+}
+void int mul(int a , int b) {
+    printf("%d * %d = %d\n",a , b, (a * b));
+}
+void int div(int a , int b) {
+    printf("%d / %d = %d\n", a, b, (a / b));
+}
+
 int main() {
+    int a = 10, b = 5;
+
+    add(a, b);
+    sub(a, b);
+    mul(a, b);
+    div(a, b);
 
     return 0;
 }
